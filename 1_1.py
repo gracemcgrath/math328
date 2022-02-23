9ae5bfeb7ee788f0868e4e9fd8bdf2f3dbbcd3f9
Grace McGrath <gracemcgrath@Graces-MacBook-Air-2.local>
2022-02-23 11:46:55 -0500

    adding some projects

---
+++ 1_1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+t = 0
+
+# initial conditions
+S = 45400
+I = 2100
+R = 2500
+
+#stepsize = h = deltat
+delta_t = 1
+print("initial conditions for t = ",t, "\n S = ", S, "\n I = ", I, "\n R = ", R)
+
+for k in range(3):
+    #System of ODE
+    s_prime = -0.00001*S*I
+    i_prime = .00001*S*I - I/14
+    r_prime = I/14
+    
+    #Approximate the rate of change 
+    delta_s = s_prime*delta_t
+    delta_i = i_prime*delta_t
+    delta_r = r_prime*delta_t
+    
+    # Update all values as the loop iterates
+    t += delta_t
+    S += delta_s
+    I += delta_i
+    R += delta_r
+    
+    print("updated conditions for t = ",t,"\n S = ", round(S,1), "\n I = ", round(I,1), "\n R = ", round(R,1))
